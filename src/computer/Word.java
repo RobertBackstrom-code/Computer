1694e27b7b954fce9d3b53759e465546c8ed6363
Ander55on <48658266+Ander55on@users.noreply.github.com>
2021-04-21 16:22:56 +0200

    third commit small change

--- src/computer/Word.java
+++ src/computer/Word.java
@@ -1,23 +1,5 @@
 package computer;
 
-/*public abstract class Word<E> implements Operand{
-	protected E value;
-	
-	public abstract boolean isEqual(Word<E> word);
-		
-	public abstract Word<E> mul(Word<E> word);
-	
-	public abstract Word<E> add(Word<E> word);
-	
-	public E getValue() {
-		return value;
-	}
-	
-	public String toString() {
-		return value.toString();
-	} 
-}*/
-
 public abstract class Word implements Operand {
 	
 	protected Number value;
